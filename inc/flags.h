6cfe7c3f30e6dce02169448d8a04e2cf311ff488
Szymon Mańka <manka543@gmail.com>
2023-11-16 15:15:25 +0100

    command prompt options detection

---
+++ inc/flags.h
@@ -0,0 +1,15 @@
+//
+// Created by manka on 16/11/2023.
+//
+
+#ifndef VIGENEREENCRYPTION_FLAGS_H
+#define VIGENEREENCRYPTION_FLAGS_H
+
+enum class flags : int {
+    encrypt = 0,
+    decrypt,
+    breakingKey,
+    error
+};
+
+#endif //VIGENEREENCRYPTION_FLAGS_H
--- main.cpp
+++ main.cpp
@@ -2,9 +2,25 @@
 #include "inc/utilities.h"
 #include "inc/encrypt.h"
 #include "inc/decrypt.h"
+#include "inc/flags.h"
 
+constexpr std::string encryptFlag = "--en";
+constexpr char decryptFlag[] = "--de";
+constexpr char breakingKeyFlag[] = "--br";
+constexpr char inputFileSwitch[] = "-i";
+constexpr char outputFileSwitch[] = "-o";
+constexpr char keyFileSwitch[] = "-k";
 
 int main(int argv, char *argc[] ) {
+    flags pickedMode {flags::error};
+    for(int i = 1; i < argv; i++){
+        if(argc[i][0] == '-'){
+            if(argc[i][1] == '-'){
+
+            }
+        }
+    }
+
     std::string text {"testowy tekst bo trzeba sprawdzic czy dziala bo to nie jest pewne a testowanie jest bardzo wazne\na tu sprawdzamy czy druga linijka tez bedzie dobrze dzialac"}, key {"alamakota"};
     text = Encrypt::encrypt(text, key);
     std::cout<<"encrypted: "<<text<<std::endl;
